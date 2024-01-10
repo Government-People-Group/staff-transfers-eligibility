f7c054a2bf9b94b6572ddae633e879b837df357c
Lakshmi Karuppiah <lakshmi.karuppiah@cabinetoffice.gov.uk>
2024-01-10 11:22:56 +0000

    List of departments view

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,53 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+// Run this code when a form is submitted to 'civil-servant-answer'
+router.post('/civil-servant-answer', function (req, res) {
+
+    // Make a variable and give it the value from 'isYourTransferPermananent'
+    var isYourTransferPermananent = req.session.data['civil-servant']
+  
+    // Check whether the variable matches a condition
+    if (isYourTransferPermananent == "yes"){
+      // Send user to next page
+      res.redirect('/transfer-permanent')
+    } else {
+      // Send user to ineligible page
+      res.redirect('/ineligible')
+    }
+})
+
+// Run this code when a form is submitted to 'transfer-permanent-answer'
+router.post('/transfer-permanent-answer', function (req, res) {
+
+    // Make a variable and give it the value from 'isYourTransferPermananent'
+    var isYourTransferPermananent = req.session.data['transfer-permanent']
+  
+    // Check whether the variable matches a condition
+    if (isYourTransferPermananent == "yes"){
+      // Send user to next page
+      res.redirect('/departments')
+    } else {
+      // Send user to ineligible page
+      res.redirect('/ineligible')
+    }
+  
+  })
+
+  // Run this code when a form is submitted to 'departments-answer'
+router.post('/departments-answer', function (req, res) {
+
+    // Make a variable and give it the value from 'isYourTransferPermananent'
+    var toandfromDepartments = req.session.data['departments']
+  
+    // Check whether the variable matches a condition
+    if (toandfromDepartments == "yes"){
+      // Send user to next page
+      res.redirect('/eligible')
+    } else {
+      // Send user to ineligible page
+      res.redirect('/ineligible')
+    }
+  
+  })
